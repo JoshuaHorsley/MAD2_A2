f86cd08cace8aec11d6c89ca41c04676dbd15e8e
Kali404 <146847748+Emerthyst404@users.noreply.github.com>
2025-12-04 14:58:09 -0500

    Commenting Completed

--- PlantCareTracker/PlantEntity+CoreDataClass.swift
+++ PlantCareTracker/PlantEntity+CoreDataClass.swift
@@ -1,14 +1,17 @@
-//
-//  PlantEntity+CoreDataClass.swift
-//  PlantCareTracker
-//
-//  Created by Student on 2025-12-01.
-//
-//
+//  FILE:        PlantDetailViewController.swift
+//  PROJECT:     PlantCareTracker
+//  COURSE:      Mobile Application Development 2
+//  DATE:        11 - 05 - 2025
+//  AUTHORS:     Josh Horsley, Will Lee, Jack Prudnikowicz, Kalina Cathcart
+//  DESCRIPTION: Data entity class that represents a plant in the PlantCareTracker app.
+
 
 import Foundation
 import CoreData
 
+// CLASS:       PlantEntity
+// DESCRIPTION: NSManagedObject subclass representing a plant entity in Core Data.
+//              Stores all information related to a single plant.
 @objc(PlantEntity)
 public class PlantEntity: NSManagedObject {
 
