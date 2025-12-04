c14b1ea8b317925257299b4a15ec59c3458682ed
Kali404 <146847748+Emerthyst404@users.noreply.github.com>
2025-12-04 15:02:15 -0500

    Commenting Completed

--- PlantCareTracker/PlantEntity+CoreDataProperties.swift
+++ PlantCareTracker/PlantEntity+CoreDataProperties.swift
@@ -1,17 +1,21 @@
-//
-//  PlantEntity+CoreDataProperties.swift
-//  PlantCareTracker
-//
-//  Created by Student on 2025-12-01.
-//
-//
-
+//  FILE:        PlantEntity+CoreDataProperties.swift
+//  PROJECT:     PlantCareTracker
+//  COURSE:      Mobile Application Development 2
+//  DATE:        12-03-2025
+//  AUTHORS:     Josh Horsley, Will Lee, Jack Prudnikowicz, Kalina Cathcart
+//  DESCRIPTION: Core Data entity extension that defines properties and fetch request for the PlantEntity managed object.
+            
 import Foundation
 import CoreData
 
-
+// EXTENSION:   PlantEntity
+// DESCRIPTION: Extends PlantEntity to provide Core Data property definitions and fetch request
 extension PlantEntity {
 
+    // FUNCTION:    fetchRequest
+    // PARAMETERS:  none
+    // RETURNS:     NSFetchRequest<PlantEntity> - A fetch request for PlantEntity objects
+    // DESCRIPTION: Creates and returns a fetch request for retrieving PlantEntity objects from Core Data
     @nonobjc public class func fetchRequest() -> NSFetchRequest<PlantEntity> {
         return NSFetchRequest<PlantEntity>(entityName: "PlantEntity")
     }
@@ -26,6 +30,8 @@ extension PlantEntity {
 
 }
 
+// EXTENSION:   PlantEntity (Identifiable)
+// DESCRIPTION: Changes the PlantEntity to the Identifiable protocol for use with SwiftUI
 extension PlantEntity : Identifiable {
 
 }
